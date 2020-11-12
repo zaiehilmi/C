348f8b5843d5b13e6d8eb17a439511e69cad2a79
Zaie Hilmi <zaiehilmi@gmail.com>
2020-11-12 22:52:24 +0800

    ngntuk so hold dulu

---
+++ lab_3/6fifosend.c
@@ -0,0 +1,21 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SAIZTIMBAL 100
+
+char *fifo = "penghantarfifo";
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
+
+
+
