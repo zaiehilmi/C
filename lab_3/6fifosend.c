d7273e5428e3444ff538a7277287e3671f9cbe25
Zaie Hilmi <zaiehilmi@gmail.com>
2020-11-16 22:04:05 +0800

    Selesai copy

--- lab_3/6fifosend.c
+++ lab_3/6fifosend.c
@@ -1,21 +1,41 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define SAIZTIMBAL 100
 
-char *fifo = "penghantarfifo";
+char *fifo = "penghantarfifo";  //fail fifo
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int main(int argc, char **argv) {
+    int fd, j, nwrite;
+    char timbal[SAIZTIMBAL];
+
+    if (argc < 2) {
+        fprintf(stderr, "Format Penggunaan: ./%s <saat> <mesej>\n", __FILE__);
+        exit(1);
+    }
+
+    fd = open(fifo, O_WRONLY | O_NONBLOCK);
+    if (fd < 0) {
+        perror("Ralat pada open()\n");
+        exit(2);
+    }
+
+    for (j = 0; j < argc; j++) {
+        if (strlen(argv[j] > SAIZTIMBAL)) {
+            printf("Mesej terlalu panjang\n");
+            continue;
+        }
+
+        strcpy(timbal, argv[j]);
+
+        nwrite = write(fd, timbal, SAIZTIMBAL - 1);
+        if (nwrite == -1) {
+            perror("Ralat pada write()");
+        }
+    }
     return 0;
 }
-
-
-
-
-
