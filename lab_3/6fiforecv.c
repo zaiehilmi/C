84ca8b4575fd9bfc6594a835d44b691ab4c935a4
Zaie Hilmi <zaiehilmi@gmail.com>
2020-11-16 23:18:00 +0800

    masih tak tau jawapan dia apa

--- lab_3/6fiforecv.c
+++ lab_3/6fiforecv.c
@@ -1,37 +1,38 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define SAIZTIMBAL 100
 
-char *fifo = "penghantaranfifo";
+char *fifo = "penghantaranfifo";  //nama fail fifo
 
-int main(int argc, char const *argv[])
-{
-    int fd; 
+int main(int argc, char const *argv[]) {
+    int fd;
     char timbal[SAIZTIMBAL];
-    
-    int binafail = mkfifo(fifo, 0766);
+
+    int binafail = mkfifo(fifo, 0766);  //bina fail fifo dgn kod akses 766
     if (binafail == -1 && errno != EEXIST) {
         perror("Ralat pada mkfifo()\n");
         exit(1);
     }
-    
+
     fd = open(fifo, O_RDWR);
-    if (fd < 0) {
+    if (fd < 0) {  //buka fail fifo untuk memulakan read and write
         perror("Ralat pada open()\n");
         exit(2);
     }
 
     for (;;) {
-        if (read(fd, timbal, SAIZTIMBAL) < 0)
+        if (read(fd, timbal, SAIZTIMBAL - 1) < 0)
             perror("Ralat pada read()\n");
 
         printf("Diterima: %s\n", timbal);
+        // if (timbal == )
+        //     exit(3);
     }
     return 0;
 }
--- lab_3/6fifosend.c
+++ lab_3/6fifosend.c
@@ -7,35 +7,36 @@
 
 #define SAIZTIMBAL 100
 
-char *fifo = "penghantarfifo";  //fail fifo
+char *fifo = "penghantaranfifo";  //fail fifo
 
 int main(int argc, char **argv) {
     int fd, j, nwrite;
     char timbal[SAIZTIMBAL];
 
     if (argc < 2) {
-        fprintf(stderr, "Format Penggunaan: ./%s <saat> <mesej>\n", __FILE__);
+        fprintf(stderr, "Format Penggunaan: ./%s <mesej1> <mesej2> ...\n", __FILE__);
         exit(1);
     }
 
-    fd = open(fifo, O_WRONLY | O_NONBLOCK);
+    fd = open(fifo, O_WRONLY | O_NONBLOCK);  //buka fail fifo
     if (fd < 0) {
         perror("Ralat pada open()\n");
         exit(2);
     }
 
     for (j = 0; j < argc; j++) {
-        if (strlen(argv[j]) > SAIZTIMBAL) {
+        if (strlen(argv[j]) > SAIZTIMBAL) {  //cek saiz mesej
             printf("Mesej terlalu panjang\n");
             continue;
         }
 
-        strcpy(timbal, argv[j]);
+        strcpy(timbal, argv[j]);  //copy setiap mesej ke timbal
 
-        nwrite = write(fd, timbal, SAIZTIMBAL - 1);
+        nwrite = write(fd, timbal, SAIZTIMBAL - 1);  //hantar mesej dari timbal ke fail fifo
         if (nwrite == -1) {
             perror("Ralat pada write()");
         }
     }
+    close(fd);
     return 0;
 }
