1ad0cc4d92c357c53b052d751fe588a039f61863
Zaie Hilmi <zaiehilmi@gmail.com>
2020-11-21 13:55:37 +0800

    improve ux

--- lab_3/2sigdefault.c
+++ lab_3/2sigdefault.c
@@ -1,25 +1,25 @@
 // Latihan 3-2
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
- 
+
 void kendaliIsyarat(int isy);
 
 int main() {
     static struct sigaction tindak;
     tindak.sa_handler = kendaliIsyarat;
 
-    printf("Sebelum sigaction(), minta tekan [CTRL]+C!\n");
+    printf("Sebelum sigaction(), tolong tekan [CTRL]+C!\n");
     sleep(5);
 
     sigaction(SIGINT, &tindak, NULL);
-    printf("Selepas sigaction(), minta tekan [CTRL]+C!\n");
+    printf("Selepas sigaction(), tolong tekan [CTRL]+C!\n");
     sleep(7);
     exit(0);
 }
 
-void kendaliIsyarat(int isy){
+void kendaliIsyarat(int isy) {
     printf("Menerima isyarat mencelah %d\n", isy);
 }
