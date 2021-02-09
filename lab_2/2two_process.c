90ba3fae2ab7123d411ee18e8205a99993d57759
Zaie Hilmi <zaiehilmi@gmail.com>
2021-02-10 00:06:31 +0800

    tambah komen

--- lab_2/2two_process.c
+++ lab_2/2two_process.c
@@ -7,20 +7,20 @@
 
 int main(int argc, char const *argv[]) {
     pid_t pid;
-    pid = fork();
+    pid = fork();  //lahirkan anak daripada proses induk
 
     switch (pid) {
-        case (-1): {
+        case (-1): {  //jika terdapat error
             perror("Terdapat masalah pada fork()");
             break;
         }
-        case (0): {
+        case (0): {  //berjaya fork dan lahir proses anak
             printf("Kes 0: \n--Proses anak = %d,\n--proses induk= %d\n", getpid(), getppid());
             break;
         }
-        default: {
+        default: {  //pid untuk proses induk
             printf("Lalai: \n--Proses anak = %d, \n--proses induk = %d\n", pid, getpid());
-            wait(NULL);
+            wait(NULL);  //tunggu child untuk mati dan NULL menunjukkan status tak perlu disimpan
             break;
         }
     }
--- lab_2/3hellocancel2.c
+++ lab_2/3hellocancel2.c
@@ -1,31 +1,32 @@
 // Exercise 2-3
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-void* benang1 (void* p);
-void* benang2 (void* p);
+void* benang1(void* p);  //fungsi yang akan dijalankan oleh t1
+void* benang2(void* p);  //fungsi yang akan dijalankan oleh t2
 
 int kira = 0;
 pthread_t sampel_benang;
 
 int main(void) {
-
+    //cipta 2 thread dan assign tugas yang akan dijalankan oleh thread tersebut dgn fungsi
     pthread_t t1, t2;
     pthread_create(&t1, NULL, benang1, NULL);
     pthread_create(&t2, NULL, benang2, NULL);
 
+    //tunggu untuk fungsi selesai dijalankan oleh dua2 thread dan keluar program bersama
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-    
+
     // return 0;
 }
 
-void* benang1 (void* p) {
+void* benang1(void* p) {  //kira sampai 5 saat dan keluar
 
-    while(1) {
+    while (1) {
         printf("Ini ialah benang 1 (Thread ID: %u)\n", pthread_self());
         sleep(1);
         kira++;
@@ -34,16 +35,14 @@ void* benang1 (void* p) {
             pthread_cancel(sampel_benang);
             pthread_exit(NULL);
         }
-        
     }
 }
 
-void* benang2 (void* p) {
+void* benang2(void* p) {
     sampel_benang = pthread_self();
 
     while (1) {
         printf("Ini ialah benang 2 (Thread ID: %u)\n", sampel_benang);
         sleep(3);
     }
-    
 }
