78850d9838f76b097c8f11f631034a819874cf91
Zaie Hilmi <zaiehilmi@gmail.com>
2021-02-10 00:22:59 +0800

    tambah komen

--- lab_2/6io-for.c
+++ lab_2/6io-for.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define SIZE 512                                 //saiz buffer
+#define SIZE 512  //saiz buffer
 
-int main(int argc, char const *argv[])
-{
-    int nread;
+int main(int argc, char const *argv[]) {
+    int nread;  //baca daripada STDIN
     char buf[SIZE];
     char mesej[] = "Masukkan input melalui papan kekunci anda:\n";
     char mesej2[] = "Ini ialah salinan input anda: \n";
 
     write(1, mesej, 44);
     for (int i = 0; (nread = read(0, buf, SIZE)) > 0; i++) {
-        write(1, mesej2, 32);
-        write(1, buf, nread); 
+        write(1, mesej2, 32);  //papar mesej2 ke STDOUT
+        write(1, buf, nread);  //papar mesej yg ditaip dari STDIN
     }
-    
+
     // while((nread = read(0, buf, SIZE)) > 0 ) {   //baca dari fd = 0 (keyboard)
     //     write(1, buf, nread);                    //papar pada fd = 1 (skrin)
     // }
